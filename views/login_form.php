1f24b21184ea8b4955d99f2a0e722f9fb7b98348
Mr.Steklyashka <mail@stek.ru>
2025-06-17 12:06:14 +0300

    17.06.25

---
+++ views/login_form.php
@@ -0,0 +1,7 @@
+<div class="login_form_container">
+    <form action="/login.php" method="post"id="abaY">
+        <input type="text" name="name" placeholder="Логин"class="login_input"><br>
+        <input type="password" name="password" placeholder="Пароль"class="login_input"><br>
+        <input type="submit" value="Войти"id=loin_APK class="login_input">
+    </form>
+</div>
