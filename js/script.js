3ea3169cba12cdd7ccda3b7e7f025db8d347ab3f
Mr.Steklyashka <mail@stek.ru>
2026-03-28 13:03:52 +0300

    29.01.26

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 let products=null;
 
+let addBusketNumber=(n)=>{
+return '<div class="in_busket_container" id="in_busket_container"><divclass="in_busket_plus"id="in_busket_plus">+</div><div class="in_busket_number" id="in_busket_number">'+n+'</div><div class="in_busket_plus"id="in_busket_minus">-</div></div>'
+}
+
+
+
+
 async function fetchPostData(url, outdata, callbackfunc){
     fetch(url,{
         method:'POST',
@@ -149,7 +156,7 @@ function createCard(name,desc,price,img,id){
         let card = document.getElementById('card');
         let newCard= card.cloneNode(true);
         newCard.style.display='block';
-        newCard.childNodes[1].style.background="url(/img/"+ img+") center /cover  no-repeat";
+        newCard.childNodes[1].style.background="url("+ img+") center /cover  no-repeat";
         newCard.childNodes[3].innerHTML=name;
         newCard.childNodes[5].innerHTML=desc;
         newCard.childNodes[7].innerHTML='<b>Цена: <b>' + price +' р.';
@@ -169,8 +176,15 @@ function addToBusket(id){
     print(id);
 }
 
-function showModal(name='',decs='',price='',img='',id = null){
+function showModal(name='',desc='',price='',img='',id = null){
     modal.style.display='block';
+    modal_image.style.background='url('+ img +') center / contain no-repeat';
+    modal_product_name.innerHTML = name;
+    modal_text.innerHTML=desc;
+    modal_price.innerHTML='Цена: <b>' + price + ' р. </b>';
+    let modalbuy = document.getElementsByClassName('modal_buy');
+    modalBuy=(modalBuy)? modalBuy[0]:null;
+    if(modal_buy) modalBuy.id='modal_buy_' + id;
 }
 
 function closeModal(){
@@ -195,12 +209,12 @@ fetchPostData('/shop/getproducts',{'limit':-1,'offset':0},showProds);
 
 
 
-createCard(product.name, product.desc, product.price, product.path, product.id);
-createCard('Монитор &laquo;samsung&raquo;','Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus dignissimos vitae animi tenetur similique praesentium, nisi libero. Deleniti, nihil.', 15000 ,'samsung-f24t450fqi-0-v1.jpg',110);
-createCard('Монитор &laquo;msi&raquo;№'+ i,'Монитор MSI PRO MP273A оборудован 27-дюймовым экраном IPS FullHD с безрамочным дизайном. Это обеспечивает реалистичность изображения и концентрацию на выполнении задач. Специальные технологии подавляют мерцание экрана и снижают интенсивность синего света, что уменьшает зрительную усталость при просмотре. Матовое покрытие устраняет световые блики.Монитор MSI PRO MP273A оснащен видеоразъемами DisplayPort, HDMI и VGA. Акустическая система с двумя динамиками мощностью по 3 Вт воспроизводит чистый звук. Конструкция настольной подставки обеспечивает простое крепление и снятие без инструментов. Крепление стандарта VESA предусматривает фиксацию мини-ПК и использование монитора в качестве моноблока. Слот в подставке может использоваться для размещения телефона и аксессуаров.', 15000 ,'001.webp',460);
-createCard('Монитор &laquo;ASrock&raquo;','Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio consequatur excepturi laboriosam officia, quidem sequi rem non quaerat eligendi magnam totam aliquid enim fugit maiores suscipit sint eaque autem ullam tempore tempora repellat. Tempore fugit cupiditate quaerat repellendus aliquid ut.', 50000 ,'i.webp',230);
-for(let i=1; i<=10; i++)
-    createCard('Монитор &laquo;msi&raquo;№'+ i,'Какое то описание', 15000 ,'samsung_F8500.webp',i);
+// createCard(product.name, product.desc, product.price, product.path, product.id);
+// createCard('Монитор &laquo;samsung&raquo;','Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus dignissimos vitae animi tenetur similique praesentium, nisi libero. Deleniti, nihil.', 15000 ,'samsung-f24t450fqi-0-v1.jpg',110);
+// createCard('Монитор &laquo;msi&raquo;№'+ i,'Монитор MSI PRO MP273A оборудован 27-дюймовым экраном IPS FullHD с безрамочным дизайном. Это обеспечивает реалистичность изображения и концентрацию на выполнении задач. Специальные технологии подавляют мерцание экрана и снижают интенсивность синего света, что уменьшает зрительную усталость при просмотре. Матовое покрытие устраняет световые блики.Монитор MSI PRO MP273A оснащен видеоразъемами DisplayPort, HDMI и VGA. Акустическая система с двумя динамиками мощностью по 3 Вт воспроизводит чистый звук. Конструкция настольной подставки обеспечивает простое крепление и снятие без инструментов. Крепление стандарта VESA предусматривает фиксацию мини-ПК и использование монитора в качестве моноблока. Слот в подставке может использоваться для размещения телефона и аксессуаров.', 15000 ,'001.webp',460);
+// createCard('Монитор &laquo;ASrock&raquo;','Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio consequatur excepturi laboriosam officia, quidem sequi rem non quaerat eligendi magnam totam aliquid enim fugit maiores suscipit sint eaque autem ullam tempore tempora repellat. Tempore fugit cupiditate quaerat repellendus aliquid ut.', 50000 ,'i.webp',230);
+// for(let i=1; i<=10; i++)
+//     createCard('Монитор &laquo;msi&raquo;№'+ i,'Какое то описание', 15000 ,'samsung_F8500.webp',i);
 
 let modal_buy = document.getElementsByClassName("modal_buy")[0];
 modal_buy.addEventListener('click',buy);
