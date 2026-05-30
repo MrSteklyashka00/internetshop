3349f5a828f0a9efc336ff1a27cf0dccd475d0ca
Mr.Steklyashka <mail@stek.ru>
2026-05-30 12:52:25 +0300

    29.01.26

--- views/user/basket.php
+++ views/user/basket.php
@@ -1,5 +1,21 @@
 <h1>Корзина</h1>
 <div class="basket_type_container">
-    <div class="basket_type basket_selected">Текущая </div>
-    <div class="basket_type">Завершенные</div>
+    <div  id="basket_type_cur"class="basket_type basket_selected">Текущая </div>
+    <div id=""basket_type_old class="basket_type">Завершенные</div>
+</div>
+
+<div class="basket_cards_container">
+
+<div class="basket_card_item">
+    <div class="basket_card_image_wraper">
+        <div class="basket_card_image"></div>
+    </div>
+    <div class="basket_card_info_wrapper">
+        <div class="basket_card_title">Картошка</div>
+        <div class="basket_card_tools_wrapper"></div>
+        <div class="basket_total_price">67&nbsp;067 ₽</div>
+        <div class="basket_container" id="basket_container"><div class="basket_plus"id="basket_plus">+</div><div class="basket_number" id="basket_number">1</div><div class="basket_plus"id="basket_minus">-</div></div>
+    </div>
+</div>
+
 </div>
